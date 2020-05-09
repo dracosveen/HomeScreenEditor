7e9cb8ca384bbb8fcfac7a3e3b59d5007108abb4
Richard Cumming <richard.cumming@outlook.com>
2020-05-09 14:20:11 +1000

    Initial Commit

---
+++ HomeScreenEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HomeScreenEditor
+//
+//  Created by Richard Cumming on 09/05/2020.
+//  Copyright © 2020 dracosveen's Studio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HomeScreenEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HomeScreenEditor
+//
+//  Created by Richard Cumming on 09/05/2020.
+//  Copyright © 2020 dracosveen's Studio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
